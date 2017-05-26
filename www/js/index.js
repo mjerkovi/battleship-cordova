57419d70e6ac259d1f12bcca645c6206a06c4f90
Marko Jerkovic <mjerkovi@ucsc.edu>
2017-05-26 16:29:31 -0700

    Some progress

--- www/js/index.js
+++ www/js/index.js
@@ -5,7 +5,7 @@ var app = function() {
     self.is_configured = false;
 
     var server_url = "https://luca-ucsc-teaching-backend.appspot.com/keystore/";
-    var call_interval = 2000;
+    var call_interval = 4000;
 
     Vue.config.silent = false; // show all warnings
 
@@ -17,7 +17,15 @@ var app = function() {
     };
 
     self.my_identity = randomString(20);
-    self.null_board = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
+
+    self.null_board =   [" ", " ", " ", " ", " ", " ", " ", " ",
+                        " ", " ", " ", " ", " ", " ", " ", " ",
+                        " ", " ", " ", " ", " ", " ", " ", " ",
+                        " ", " ", " ", " ", " ", " ", " ", " ",
+                        " ", " ", " ", " ", " ", " ", " ", " ",
+                        " ", " ", " ", " ", " ", " ", " ", " ",
+                        " ", " ", " ", " ", " ", " ", " ", " ",
+                        " ", " ", " ", " ", " ", " ", " ", " "];
 
     // Enumerates an array.
     var enumerate = function(v) {
@@ -42,11 +50,9 @@ var app = function() {
         self.is_configured = true;
     };
 
-    // This is the object that contains the information coming from the server.
-    self.player_x = null;
-    self.player_o = null;
+    self.player_1 = null;
+    self.player_2 = null;
 
-    // This is the main control loop.
     function call_server() {
         console.log("Calling the server");
         if (self.vue.chosen_magic_word === null) {
@@ -71,137 +77,212 @@ var app = function() {
                 key: self.vue.chosen_magic_word,
                 val: JSON.stringify(
                     {
-                        'player_x': self.player_x,
-                        'player_o': self.player_o,
-                        'board': self.vue.board
+                        'player_1': self.player_1,
+                        'player_2': self.player_2,
+                        'player1_board': self.vue.player1_board,
+                        'player2_board': self.vue.player2_board
                     }
                 )
             }
         );
     };
 
-
-    // Main place where we receive data and act on it.
     self.process_server_data = function (data) {
-        // If data is null, we send our data.
+        console.log("My ident: " + self.my_identity);
         if (!data.result) {
-            self.player_x = self.my_identity;
-            self.player_o = null;
-            self.vue.board = self.null_board;
+            self.player_1 = self.my_identity;
+            self.player_2 = null;
+            self.vue.player1_board = getBoard();
+            self.vue.player2_board = self.null_board;
             self.vue.is_my_turn = false;
             self.send_state();
-        } else {
-            // I technically don't need to assign this to self, but it helps debug the code.
+        }
+        // the server already has the magic word stored so the result is not null.
+        // although may not be "our" game if there was a conflict with the magic word.
+        else {
             self.server_answer = JSON.parse(data.result);
-            self.player_x = self.server_answer.player_x;
-            self.player_o = self.server_answer.player_o;
-            if (self.player_x === null || self.player_o === null) {
-                // Some player is missing. We cannot play yet.
+            console.log(self.server_answer.player1_board);
+            console.log(self.server_answer.player2_board);
+            self.player_1 = self.server_answer.player_1;
+            self.player_2 = self.server_answer.player_2;
+            //one or both of the players are missing
+            if (self.player_1 === null || self.player_2 === null) {
                 self.vue.is_my_turn = false;
-                console.log("Not all players present.");
-                if (self.player_o === self.my_identity || self.player_x === self.my_identity) {
-                    // We are already present, nothing to do.
+                if (self.player_1 === self.my_identity || self.player_2 === self.my_identity) {
                     console.log("Waiting for other player to join");
                 } else {
                     console.log("Signing up now.");
-                    // We are not present.  Let's join if we can.
-                    if (self.player_x === null) {
-                        // Preferentially we play as x.
-                        self.player_x = self.my_identity;
+                    if (self.player_1 === null) {
+                        self.player_1 = self.my_identity;
+                        self.vue.player1_board = getBoard();
+                        self.vue.player2_board = self.server_answer.player2_board;
                         self.send_state();
-                    } else if (self.player_o === null) {
-                        self.player_o = self.my_identity;
+                    } else if (self.player_2 === null) {
+                        self.player_2 = self.my_identity;
+                        // i am now player_2 so i must set up my board and push it to the server
+                        self.vue.player2_board = getBoard();
+                        self.vue.player1_board = self.server_answer.player1_board;
                         self.send_state();
                     } else {
-                        // The magic word is already taken.
+                        console.log("Oops intruding1");
                         self.vue.need_new_magic_word = true;
                     }
                 }
-            } else {
-                console.log("Both players are present");
-                // Both players are present.
-                // Let us determine our role if any.
-                if (self.player_o !== self.my_identity && self.player_x !== self.my_identity) {
-                    // Again, we are intruding in a game.
+            }
+            // both players are there
+            else {
+                // but i am not one of the players, so i must be
+                // so i must be intruding on someone elses game
+                if (self.player_1 !== self.my_identity && self.player_2 !== self.my_identity) {
+                    console.log("Oops intruding2");
                     self.vue.need_new_magic_word = true;
                 } else {
-                    // Here is the interesting code: we are playing, and the opponent is there.
-                    // Reconciles the state.
+                    // okay now i am in the appropriate game and both players are here
                     self.update_local_vars(self.server_answer);
                 }
             }
         }
-    };
+    }
 
     self.update_local_vars = function (server_answer) {
-        // First, figures out our role.
-        if (server_answer.player_o === self.my_identity) {
-            self.vue.my_role = 'o';
-        } else if (server_answer.player_x === self.my_identity) {
-            self.vue.my_role = 'x';
+        if (server_answer.player_1 === self.my_identity) {
+            self.vue.my_role = "player_1";
+        } else if (server_answer.player_2 === self.my_identity) {
+            self.vue.my_role = "player_2";
         } else {
-            self.vue.my_role = ' ';
+            self.vue.my_role = "";
         }
 
-        // Reconciles the board, and computes whose turn it is.
-        for (var i = 0; i < 9; i++) {
-            if (self.vue.board[i] === ' ' || server_answer.board[i] !== ' ') {
-                // The server has new information for this board.
-                Vue.set(self.vue.board, i, server_answer.board[i]);
-            } else if (self.vue.board[i] !== server_answer.board[i]
-                && self.vue.board[i] !== ' ' && server_answer.board[i] !== ' ')  {
-                console.log("Board inconsistency at: " + i);
-                console.log("Local:" + self.vue.board[i]);
-                console.log("Server:" + server_answer.board[i]);
-            }
-        }
-
-        // Compute whether it's my turn on the basis of the now reconciled board.
-        self.vue.is_my_turn = (self.vue.board !== null) &&
-            (self.vue.my_role === whose_turn(self.vue.board));
-    };
-
-
-    function whose_turn(board) {
-        num_x = 0;
-        num_o = 0;
-        for (var i = 0; i < 9; i++) {
-            if (board[i] === 'x') num_x += 1;
-            if (board[i] === 'o') num_o += 1;
-        }
-        if (num_o >= num_x) {
-            return 'x';
-        } else {
-            return 'o';
-        }
+        //********* change this
+        self.vue.player1_board = self.server_answer.player1_board;
+        self.vue.player2_board = self.server_answer.player2_board;
     }
 
 
+
+    self.magic_word_prefix = "lololyadayada596";
+
     self.set_magic_word = function () {
-        self.vue.chosen_magic_word = self.vue.magic_word;
+        self.vue.chosen_magic_word = self.magic_word_prefix.concat(self.vue.magic_word);
         self.vue.need_new_magic_word = false;
         // Resets board and turn.
         self.vue.board = self.null_board;
         self.vue.is_my_turn = false;
-        self.vue.my_role = "";
+        self.vue.my_identity = "";
     };
 
-    self.play = function (i, j) {
-        // Check that the game is ongoing and that it's our turn to play.
-        if (!self.vue.is_my_turn) {
-            return;
+        //checks for valid placement of ship of ship_size in a board_size x board_size at (x,y) with orientatation (0->horizontal, 1-> vertical)
+    function isvalid(board, x, y, orientation, ship_size, board_size){
+        if(orientation){
+            if(x+ship_size >= board_size) return false;
+            for(var i = x; i < x+ship_size; i++){
+                if(board[i][y] != '*' ||
+                    (y-1 >= 0 && board[i][y-1] != '*') || // to ensure that ships do not "touch each other"
+                    (y+1 < board_size && board[i][y+1] != '*'))
+                        return false;
+            }
+            if((x - 1 >= 0 && board[x-1][y] != '*') ||
+                (x + ship_size < board_size && board[x+ship_size][y] != '*')) return false;
+        } else {
+            if(y+ship_size >= board_size) return false;
+            for(var i = y; i < y+ship_size; i++){
+                if(board[x][i] != '*' ||
+                    (x-1 >= 0 && board[x-1][i] != '*') || // to ensure that ships do not "touch each other"
+                    (x+1 < board_size && board[x+1][i] != '*'))
+                        return false;
+            }
+            if((y-1 >= 0 && board[x][y-1] != '*') ||
+                (y+ship_size < board_size && board[x][y+ship_size] != '*')) return false;
         }
-        // Check also that the square is empty.
-        if (self.vue.board[i * 3 + j] !== ' ') {
-            return;
-        }
-        // Update self.vue.board.
-        Vue.set(self.vue.board, i * 3 + j, self.vue.my_role);
-        // We have already played.
-        self.vue.is_my_turn = false;
-        self.send_state();
-    };
+        return true;
+    }
 
+    function print(board){
+        var size = Math.sqrt(board.length);
+        for(var i = 0; i < size; i++){
+            var s = "";
+            for(var j = 0; j < size; j++){
+                s += board[i*size + j];
+            }
+            console.log(s);
+        }
+    }
+
+    //creates a ship in board with shipid
+    function setShip(board, orientation, x, y, ship_size, shipid){
+        if(orientation){
+            for(var i = x; i < x+ship_size; i++){
+                board[i][y] = shipid;
+            }
+        }else{
+            for(var i = y; i < y+ship_size; i++){
+                board[x][i] = shipid;
+            }
+        }
+    }
+
+    //get random integers in range [Min, Max]
+    function get_random(Min, Max){
+        return Math.floor(Math.random() * (Max - Min +1)) + Min;
+    }
+
+    //create a ship
+    function createShip(board, board_size, ship_size, shipid){
+        var counter=0;
+        while(counter < 200){
+            counter++;
+            var orientation = get_random(0, 1);//0 -> horizontal, 1-> vertical
+            var x=0;
+            var y=0;
+            if(orientation){
+                x = get_random(0, board_size-ship_size-1);
+                y = get_random(0, board_size-1);
+            }else{
+                x = get_random(0, board_size-1);
+                y = get_random(0, board_size-ship_size-1);
+            }
+            if(!isvalid(board, x, y, orientation, ship_size, board_size)) continue; //check if it conflicts
+            setShip(board, orientation, x, y, ship_size, shipid);
+            break;
+        }
+    }
+
+    //create all ships
+    function createShips(board, board_size){
+        var ships = [[1,3], [3,1], [2,2]]; // first element of every pair is number of ships, second element is length of ship
+        var shipid = 1;
+        for(var i = 0; i < ships.length; i++){
+            for(var count = 0; count < ships[i][0]; count++){
+                createShip(board, board_size, ships[i][1], shipid);
+                shipid++;
+            }
+        }
+    }
+
+    //flatten 2d vector to 1d vector
+    function flatten(board){
+        var size = board.length;
+        var board2 = new Array(size*size);
+        for(var i = 0; i < size; i++){
+            for(var j = 0; j < size; j++)
+                board2[i*size + j] = board[i][j];
+        }
+        return board2;
+    }
+
+    // get 8x8 board flattened
+    function getBoard(){
+        var size = 8;
+        var board = new Array(size);
+        for (var i = 0; i < size; i++) {
+            board[i] = new Array(size);
+            for (var j = 0; j < size; j++)
+                board[i][j] = '*';
+        }
+        createShips(board, size);
+        board = flatten(board);
+        return board;
+    }
 
     self.vue = new Vue({
         el: "#vue-div",
@@ -212,13 +293,17 @@ var app = function() {
             chosen_magic_word: null,
             need_new_magic_word: false,
             my_role: "",
-            board: self.null_board,
+            my_board: self.null_board,
+            opponent_board: self.null_board,
+            player1_board: self.null_board,
+            player2_board: self.null_board,
             is_other_present: false,
             is_my_turn: false
         },
         methods: {
-            set_magic_word: self.set_magic_word,
-            play: self.play
+            set_magic_word: self.set_magic_word
+            //play: self.play
+
         }
 
     });
